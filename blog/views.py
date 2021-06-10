7c2a0348cde1c206bfe1b3394a2b73992405c435
MeowGuaurr <reynitamercedes@gmail.com>
2021-06-10 00:34:28 -0600

    :working on users profile

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'about'})
 
-#dictionary for news posts
-posts = [
-    { 
-        'author': 'Reyna',
-        'title': 'Test1',
-        'content': 'Test content',
-        'date_posted': 'today'
-    },
-    { 
-        'author': 'Mercedes',
-        'title': 'Test2',
-        'content': 'Test content2',
-        'date_posted': 'tomorrow'
-    }
-]
